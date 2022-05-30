c4bbf832b39b415d07b8998fdc35b9b79bd817a4
alicia-cordial <71872114+alicia-cordial@users.noreply.github.com>
2022-05-30 12:30:38 +0200

    pusher avatar

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -127,6 +127,8 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
     /**
      * @ORM\Column(type="string", length=255)
+     * 
+     *  @Groups({"user:read", "user:write", "experience:read"})
      */
     private $avatar;
 
